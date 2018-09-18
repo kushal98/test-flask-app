c192f9f2d00d5ae6d352539d47a9ad98206ea847
Kushal Agrawal <kagrawal61@gmail.com>
2018-09-18 23:17:29 +0530

    authenticate working

---
+++ test-login/app.py
@@ -0,0 +1,29 @@
+from flask import Flask,request
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Kushal@123'
+app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+@app.route("/")
+def hello():
+    return "Welcome to python flask application"
+
+@app.route("/auth")
+def authenticate():
+    username = request.args.get('UserName')
+    password = request.args.get('Password')
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
+    data = cursor.fetchone()
+    if data is None:
+     return "Username or Password is wrong"
+    else:
+     return "Logged in successfully"
+
+if __name__ == "__main__":
+    app.run()
